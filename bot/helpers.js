f30f69d4e7755a125c0a25a7a34d6683df33f8b8
MSFTserver <sawyerlemmo@gmail.com>
2017-10-28 14:40:51 -0700

    update helpers

--- bot/helpers.js
+++ bot/helpers.js
@@ -1,9 +1,18 @@
 let config = require('config');
 let permRanks = config.get('moderation');
+let speechChannels = config.get('speechbot');
 
 // Checks if user is allowed to use a command only for mods/team members
 exports.hasPerms = function(msg){
 if(msg.member.roles.some(r=>permRanks.perms.includes(r.name)) ) {
+  return true;
+} else {
+  return false;
+}
+}
+
+exports.hasSpeechChannels = function(msg){
+if(speechChannels.channelIDs.includes(msg.channel.id) ) {
   return true;
 } else {
   return false;
