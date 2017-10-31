220f43bd3519f8bed3410c3048a6b0de34dc5aaa
MSFTserver <sawyerlemmo@gmail.com>
2017-10-31 14:40:09 -0700

    remove example and split into two example files

--- bot/modules/exampleBot.js
+++ bot/modules/exampleBot.js
@@ -3,9 +3,6 @@ exports.commands = [
 	"demo" // command that is in this file, every command needs it own export as shown below
 ]
 
-exports.custom = [
-    "initFunction"
-]
 
 exports.demo = {
 	usage: "<subcommand>",
@@ -15,8 +12,4 @@ exports.demo = {
     }
 }
 
-exports.initFunction = function(bot) {
-    // Other functions that needs to be ran once on bootup!
-    // For example a timed function and or some init stuff..
-}
 */
